b6c72a02261d6997749f9636054ec9c67288f0aa
craigb <BrandonCraig@rentalcars.com>
2020-04-18 13:52:04 +0100

    Manual Dependency Injection

---
+++ src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertyInjectedControllerTest {
+
+    PropertyInjectedController controller;
+
+    @BeforeEach
+    void setup() {
+        controller = new PropertyInjectedController();
+
+        controller.greetingService  = new GreetingServiceImpl();
+
+    }
+
+    @Test
+    void sayGreeting() {
+
+        System.out.println(controller.getGreeting());
+
+    }
+
+}
